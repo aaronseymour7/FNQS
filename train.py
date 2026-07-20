87e674bc321d7c70596394a9a1b5e88bbd7f6282
aaronseymour7 <adseymour22@gmail.com>
2026-07-19 21:10:23 -0400

    Increase training iterations from 100 to 1200
    Increased the number of iterations for training from 100 to 1200.

--- train.py
+++ train.py
@@ -161,7 +161,7 @@ class FNQS1D(nn.Module):
 # --------------------------------------------------------------------------
 R = 9                        # number of couplings sampled per SR step ("systems")
 M_PER_SYSTEM = 512           # MC samples per system  ->  total M = R * M_PER_SYSTEM
-N_ITERS = 100
+N_ITERS = 1200
 LR = 0.02
 J2_LOW, J2_HIGH = 0.0, 0.6   # support of P(j2) the model is trained to cover
 
